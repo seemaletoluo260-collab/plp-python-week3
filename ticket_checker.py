b7aedb83f9582b7dfeef8bed0e29c1a643ab9349
seemaletoluo260-collab <seemaletoluo260@gmail.com>
2026-07-17 17:00:41 +0300

    Update ticket_checker.py

--- ticket_checker.py
+++ ticket_checker.py
@@ -1,1 +1,21 @@
+# ticket_checker.py
 
+print("=== Ticket Checker ===")
+
+age = int(input("Enter your age: "))
+
+student = input("Are you a student? (yes/no): ").lower()
+
+is_student = student == "yes"
+
+if age < 12:
+    print("Child Ticket")
+
+elif age >= 60:
+    print("Senior Ticket")
+
+elif is_student:
+    print("Student Ticket")
+
+else:
+    print("Adult Ticket")
